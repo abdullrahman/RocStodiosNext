506a5f9a88d1509fc7c91ac3c28bf8b17a8cc1fe
abdullrahman <aljizzani.abdullrahman@gmail.com>
2023-06-15 13:29:11 +0300

    Add ProgressBar To UploadFile

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -1,32 +1,67 @@
 import { React, useState, useEffect } from 'react'
 import { v4 as uuid } from 'uuid'
 import { storage, db } from '@/firebase/config'
-import { ref, getDownloadURL, listAll, uploadBytes } from 'firebase/storage'
+import {
+  ref,
+  getDownloadURL,
+  listAll,
+  uploadBytes,
+  uploadBytesResumable,
+} from 'firebase/storage'
 import { collection, addDoc } from 'firebase/firestore'
 
 import { useImgContext } from '@/hooks/useImgContext'
 import { useAuthContext } from '@/hooks/useAuthContext'
 
 export default function FileUpload() {
-  const { imgList, setImgList } = useImgContext()
+  const { setImgList } = useImgContext()
   const { user, authIsReady } = useAuthContext()
-  const unique_id = uuid()
 
   const [imageUpload, setImageUpload] = useState()
-  const imageRef = ref(storage, 'images/')
-  const [newUrl, setNewUrl] = useState(null)
+  const [progress, setProgress] = useState(0)
 
-  const uploadFile = async () => {
+  const uploadFile = () => {
     if (!imageUpload) return
     const imageReff = ref(storage, 'images/' + imageUpload.name)
-    uploadBytes(imageReff, imageUpload).then(async (snapshot) => {
-      getDownloadURL(snapshot.ref).then((url) => {
-        setImgList((prev) => [...prev, { url: url }])
-        addImg(url, imageUpload.name)
-      })
-      alert('upload done')
-    })
+    const uploadTask = uploadBytesResumable(imageReff, imageUpload)
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+
+        setProgress(progress) // to show progress upload
+        console.log(progress)
+
+        switch (snapshot.state) {
+          case 'paused':
+            console.log('Upload is paused')
+            break
+          case 'running':
+            console.log('Upload is running')
+            break
+        }
+      },
+      (error) => {
+        message.error(error.message)
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+          setImgList((prev) => [...prev, { url: url }])
+          addImg(url, imageUpload.name)
+        })
+      }
+    )
+    // uploadBytes(imageReff, imageUpload).then((snapshot) => {
+    //   setProgress((snapshot.bytes.Transferred / snapshot.totalBytes) * 100)
+    //   getDownloadURL(snapshot.ref).then((url) => {
+    //     setImgList((prev) => [...prev, { url: url }])
+    //     addImg(url, imageUpload.name)
+    //   })
+    //   alert('upload done')
+    //   console.log(progress)
+    // })
     setImageUpload(null)
+    setProgress(0)
   }
   const addImg = async (url, name) => {
     await addDoc(collection(db, 'Products'), {
@@ -36,19 +71,6 @@ export default function FileUpload() {
     })
   }
 
-  useEffect(() => {
-    // listAll(imageRef).then((res) => {
-    //   res.items.forEach((item) => {
-    //     getDownloadURL(item).then((url) => {
-    //       setImgList((prev) => [
-    //         ...prev,
-    //         { url: url, name: item._location.path_ },
-    //       ])
-    //       console.log(imgList)
-    //     })
-    //   })
-    // })
-  }, [])
   return (
     <>
       {authIsReady && user && (
@@ -81,15 +103,36 @@ export default function FileUpload() {
                 </label>
                 <p className="pl-1">or drag and drop</p>
               </div>
-              <p className="text-xs leading-5 text-gray-600">
+              <p className="my-1 text-xs leading-5 text-gray-600">
                 PNG, JPG, GIF up to 10MB
               </p>
               <button
                 type="button"
-                className="mt-3 rounded-md bg-white px-2.5 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                className="mr-2 mt-2 inline-flex cursor-pointer items-center rounded-lg bg-green-900 px-5 py-1.5 text-center text-sm font-medium text-white hover:bg-green-800 focus:ring-4 focus:ring-green-700 dark:bg-green-900 dark:hover:bg-green-800 "
                 onClick={uploadFile}
               >
-                Upload
+                {progress > 0 && progress < 100 && (
+                  <svg
+                    aria-hidden="true"
+                    role="status"
+                    className="mr-3 inline h-4 w-4 animate-spin text-white"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="#E5E7EB"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentColor"
+                    />
+                  </svg>
+                )}
+                {progress > 0 && progress < 100
+                  ? `Uploading... (${progress | 0})%`
+                  : 'UPLOAD'}
               </button>
             </div>
           </div>
